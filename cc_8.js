d23f784cea9b80d83501a79af2fff186bc235749
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-16 13:48:38 -0500

    Task 5: Loan Payment Calculation

--- cc_8.js
+++ cc_8.js
@@ -46,3 +46,14 @@ console.log(calculateRentalCost(5, "Luxury", false)); // Expected output: "Total
 
 // Computed the total rental cost using three parameters.
 
+// Task 5: Loan Payment Calculation
+
+function calculateLoanPayment(principal, rate, time) {
+    let totalPayment = principal + (principal * rate * time);
+    return `Total Payment: $${totalPayment.toFixed(2)}`;
+}
+console.log(calculateLoanPayment(1000, 0.05, 2)); // Expected output: "Total Payment: $1100.00"
+console.log(calculateLoanPayment(5000, 0.07, 3)); // Expected output: "Total Payment: $6050.00"
+ 
+// Created a function to see the total payment for a loan including interest over a given period
+
